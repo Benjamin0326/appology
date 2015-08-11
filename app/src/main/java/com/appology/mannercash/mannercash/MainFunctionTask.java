ca50abc5ad848b1f45ea086199344010f5131e87
iiarbutusii <iiarbutusii@68525a00-6f16-0510-9f3d-eb71219e7a77>
2015-08-11 07:17:34 +0000

    제한속도 기능 구현 시작

--- app/src/main/java/com/appology/mannercash/mannercash/MainFunctionTask.java
+++ app/src/main/java/com/appology/mannercash/mannercash/MainFunctionTask.java
@@ -23,14 +23,17 @@ public class MainFunctionTask extends AsyncTask<Void, Integer, Void> {
     LocationManager locationManager;
     GpsManager locationListener;
 
+    double prevLat = 0;
+    double prevLon = 0;
+    double curLat = 0;
+    double curLon = 0;
+
     boolean gpsOffFlag = false;
 
     Data[] data = new Data[446];
     IC[] ic = new IC[36];
     JCT[] jct = new JCT[228];
 
-    int index; // index = 진입한 Data정보와 일치하는 index값 저장
-    int flag=0; //flag는 현재 IC or JCT를 진입했는지 벗어났는지 chk
 
     public MainFunctionTask(Context mContext, LocationManager locationManager, TextView debugTextView, Data[] data, IC[] ic, JCT[] jct) {
         this.mContext = mContext;
@@ -39,7 +42,6 @@ public class MainFunctionTask extends AsyncTask<Void, Integer, Void> {
         this.data = data;
         this.ic = ic;
         this.jct=jct;
-
 
         SharedPreferences settings = mContext.getSharedPreferences("MannerCash", mContext.MODE_PRIVATE);
         id=settings.getString("email", "email");
@@ -60,84 +62,17 @@ public class MainFunctionTask extends AsyncTask<Void, Integer, Void> {
                 ((Activity) mContext).finish();
             }*/
 
-            publishProgress();
-            /*
-            if(flag==0) {
-                if(index!=999) {
-                    for (int i = 0; i < 407; i++) {
-                        if (data[i].Enter(locationListener.getLatitude(), locationListener.getLongitude())) { // IC or JCT 에 진입했는지 알아봄
-                            //Toast.makeText(mContext.getApplicationContext(), "Entered "+data[i].GetrouteName()+data[i].GetrouteNo()+data[i].GeticName()+data[i].GeticCode(), Toast.LENGTH_LONG).show();
-                            index = i;
-                            flag = 1;
-                            break;
-                        }
-                    }
-                }
-                else{
-                    for (int i = 0; i < 407; i++) {
-                        if(i==index)
-                            continue;
-                        else {
-                            if (data[i].Enter(locationListener.getLatitude(), locationListener.getLongitude())) { // IC or JCT 에 진입했는지 알아봄
-                                //Toast.makeText(mContext.getApplicationContext(), "Entered "+data[i].GetrouteName()+data[i].GetrouteNo()+data[i].GeticName()+data[i].GeticCode(), Toast.LENGTH_LONG).show();
-                                index = i;
-                                flag = 1;
-                                break;
-                            }
-                        }
-                    }
-                }
+            if(locationListener.getLatitude() != curLat && locationListener.getLongitude() != curLon) {
+                prevLat = curLat;
+                prevLon = curLon;
+                curLat = locationListener.getLatitude();
+                curLon = locationListener.getLongitude();
             }
-            else if(flag==1) {
-                if(locationListener.getMSpeed()>100){
-                    //제한속도를 지키지 못한 경우 (임의로 기준 속도를 100km/h 로 둠)
-                    Toast.makeText(mContext.getApplicationContext(), "제한속도 초과, 포인트 적립 실패", Toast.LENGTH_SHORT).show();
-                    flag=0;     // flag 및 index 초기화
-                    index=999;
-                }
-                for (int i = 0; i < 407; i++) {
-                    if (i == index)
-                        continue;
-                    else if (data[i].Enter(locationListener.getLatitude(), locationListener.getLongitude())) { // 다른 IC or JCT 에 진입했는지 알아봄
-                        Toast.makeText(mContext.getApplicationContext(), "포인트가 적립되었습니다.", Toast.LENGTH_SHORT).show();
-                            //계속 속도를 잘 유지했으면 point 적립
 
-                        /**************************************************************************************************************************************/
-                       /*
-                        try{
-                            String link = "http://10.0.2.2/mannercash_server.php?code=3&id=" +
-                                    URLEncoder.encode(id, "UTF-8")+"&password=" +
-                                    URLEncoder.encode(password, "UTF-8") + "&phoneNum=" +
-                                    URLEncoder.encode("", "UTF-8") + "&cardNum=" +
-                                    URLEncoder.encode("", "UTF-8") +"&point=" +
-                                    URLEncoder.encode("100", "UTF-8");
-                            URL url = new URL(link);
-                            HttpClient client = new DefaultHttpClient();
-                            HttpGet request = new HttpGet();
-                            request.setURI(new URI(link));
-                            HttpResponse response = client.execute(request);
-                        }
-                        catch(Exception e){
-                            Toast.makeText(mContext.getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                        /**************************************************************************************************************************************/
-/*
-                        char chk=data[index].GeticName().charAt(data[index].GeticName().length());
-                                // IcName 의 끝이 C일 경우 IC, T일 경우 JCT 이므로
-                        if(chk=='C'){  // IC통과 시 고속도로를 벗어나게 된 것이므로 index 초기화 및 flag=0으로 set
-                            flag=0;
-                        }
-                        else if(chk=='T'){  // JCT 통과 시 고속도로를 다시 달리는 것이므로 index 재설정 및 flag 1로 유지
-                            index=i;
-                            flag=1;
-                        }
-                        break;
-                    }
-                }
-            }
-            */
+
+            publishProgress();
             try {
-                //Thread.sleep(2000);
+                Thread.sleep(500);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -145,7 +80,75 @@ public class MainFunctionTask extends AsyncTask<Void, Integer, Void> {
         return null;
     }
 
- /*   private void request(String urlStr) {
+    StringBuilder sb = new StringBuilder("");
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        sb.append("GPS Enabled" + "\n" + locationListener.isProviderEnabled() + "\n");
+        sb.append("GPS 수신 상태" + "\n" + locationListener.getGpsStatus() + "\n");
+        sb.append("prevLat:" + prevLat + "\n");
+        sb.append("prevLon:" + prevLon + "\n");
+        sb.append("curLat:" + curLat + "\n");
+        sb.append("curLon:" + curLon + "\n");
+        sb.append("위도:" + String.valueOf(locationListener.getLatitude()) + "\n");
+        sb.append("경도:" + String.valueOf(locationListener.getLongitude()) + "\n");
+        sb.append("속도:" + String.valueOf(locationListener.getMSpeed()) + "\n");
+        debugTextView.setText(sb.toString());
+        sb.setLength(0);
+    }
+
+    boolean Enter(int code, Double x, Double y){    //code==0 : IC, code==1 : JCT
+        char flag;
+        if(code==0)
+            flag='C';
+        else
+            flag='T';
+
+        for(int i=0;i<446;i++){
+            if(data[i].icName.charAt(data[i].icName.length()-1) != flag)
+                continue;
+            if(data[i].Enter(x,y)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void soundTurnOn(int resId) {
+        SoundPool soundPool;
+        int soundId;
+
+        soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        soundId = soundPool.load(mContext, resId, 1);
+
+        soundPool.play(soundId, 1, 1, 0, 0, 1);
+    }
+
+    private void gpsConfiguration() {
+        locationListener = new GpsManager();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 100, locationListener);
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationListener.setIsProviderEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onPreExecute() {
+        gpsConfiguration();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+    }
+
+    @Override
+    protected void onCancelled() {
+        if(gpsOffFlag) {
+            Toast.makeText(mContext, "위치 서비스(GPS) 기능이 꺼졌습니다.\n사용 허용 후 다시 시작해주세요.", Toast.LENGTH_LONG).show();
+        }
+        locationManager.removeUpdates(locationListener);
+    }
+
+    /*private void request(String urlStr) {
         try {
             URL url = new URL(urlStr);
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
@@ -163,69 +166,4 @@ public class MainFunctionTask extends AsyncTask<Void, Integer, Void> {
             ex.printStackTrace();
         }
     }*/
-
-    private void gpsConfiguration() {
-        locationListener = new GpsManager();   // 속도 테스트
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, locationListener);   // 속도 테스트
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationListener.setIsProviderEnabled(true);
-        }
-    }
-
-    void soundTurnOn(int resId) {
-        SoundPool soundPool;
-        int soundId;
-
-        soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
-        soundId = soundPool.load(mContext, resId, 1);
-
-        soundPool.play(soundId, 1, 1, 0, 0, 1);
-    }
-
-    StringBuilder sb = new StringBuilder("");
-    @Override
-    protected void onProgressUpdate(Integer... values) {
-        sb.append("GPS Enabled" + "\n" + locationListener.isProviderEnabled() + "\n");
-        sb.append("GPS 수신 상태" + "\n" + locationListener.getGpsStatus() + "\n");
-        sb.append("위도:" + String.valueOf(locationListener.getLatitude()) + "\n");
-        sb.append("경도:" + String.valueOf(locationListener.getLongitude()) + "\n");
-        sb.append("속도:" + String.valueOf(locationListener.getMSpeed()) + "\n");
-        debugTextView.setText(sb.toString());
-        sb.setLength(0);
-    }
-
-    @Override
-    protected void onPreExecute() {
-        gpsConfiguration();   // 속도 테스트
-    }
-
-    @Override
-    protected void onPostExecute(Void aVoid) {
-    }
-
-    @Override
-    protected void onCancelled() {
-        if(gpsOffFlag) {
-            Toast.makeText(mContext, "위치 서비스(GPS) 기능이 꺼졌습니다.\n사용 허용 후 다시 시작해주세요.", Toast.LENGTH_LONG).show();
-        }
-        locationManager.removeUpdates(locationListener);
-    }
-
-
-    boolean Enter(int code, Double x, Double y){    //code==0 : IC, code==1 : JCT
-        char flag;
-        if(code==0)
-            flag='C';
-        else
-            flag='T';
-
-        for(int i=0;i<446;i++){
-            if(data[i].icName.charAt(data[i].icName.length()-1)!=flag)
-                continue;
-            if(data[i].Enter(x,y)){
-                return true;
-            }
-        }
-        return false;
-    }
 }
